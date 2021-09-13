84a46f25ca5f0ae3ec1a1e52f2ea4638118a455b
StevenD <dorvilsteven@gmail.com>
2021-09-13 01:14:30 -0400

    continuing timeblocks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,11 @@
+// displays date in the header inside if <p>
+// with an id of currentDay
 $("#currentDay").text(
+    // moment.js current date and formatting
     `${moment().format("dddd, MMM Do YYYY")}`
 );
+
+
 
 $(".row").on('click', '#task-text', function() {
     console.log('test');
