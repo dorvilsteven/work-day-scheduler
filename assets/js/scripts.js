0e3a51563620e4f69e95195b7390889523511d2f
StevenD <dorvilsteven@gmail.com>
2021-09-13 20:22:39 -0400

    time blocks and past present future functionality are active

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,13 @@
+var auditTimeBlock = function(timeBlockEl) {
+    $(timeBlockEl).removeClass("past present future");
+    if (parseInt($(timeBlockEl).attr('data-time-stamp')) === parseInt(moment().format('h'))) {
+        $(timeBlockEl).addClass('present');
+    } else if (parseInt($(timeBlockEl).attr('data-time-stamp')) < parseInt(moment().format('h'))) {
+        $(timeBlockEl).addClass('past');
+    } else if (parseInt($(timeBlockEl).attr('data-time-stamp')) > parseInt(moment().format('h'))) {
+        $(timeBlockEl).addClass('future');
+    }
+};
 // displays date in the header inside if <p>
 // with an id of currentDay
 $("#currentDay").text(
@@ -10,10 +20,21 @@ $(".row").on('click', '#task-text', function() {
     console.log('test');
 });
 
+$("#save-task").on('click', 'i', function() {
+    console.log('save');
+});
+
 // function runs every hour to see if timeblock is in
 // past, present or future, and updates accordingly
-// setInterval(function() {
-//     $(".time-block").each(function() {
-
-//     });
-// }, (1000 * 60) * 60);
+setInterval(function() {
+    $('.time-block').each(function(index, el) {
+        if (parseInt(moment().format('h')) > 5) {
+            // if the work day is over dont run the audit
+            return false;
+        } else {
+            // if the work day is active, 
+            // run the audit
+            auditTimeBlock(el); 
+        } 
+    });
+}, ((1000 * 60) * 60));
